63c6cc52645cc7221eed2af5fdff909fb7853fb2
chinadarren <chinadarren@outlook.com>
2015-09-16 15:28:44 +0800

    ss

--- app/src/main/java/com/example/cl/crazyandroiddemo5/MainActivity.java
+++ app/src/main/java/com/example/cl/crazyandroiddemo5/MainActivity.java
@@ -1,38 +1,63 @@
 package com.example.cl.crazyandroiddemo5;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.os.Handler;
+import android.os.Message;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.TextView;
 
+import java.util.Timer;
+import java.util.TimerTask;
 
-public class MainActivity extends ActionBarActivity {
-
-    @Override
+/**
+ * 霓虹灯效果
+ */
+public class MainActivity extends Activity {
+private int currentColor = 0;
+    //定义一个颜色数组
+    final int [] colors = new int []{
+            android.R.color.holo_red_dark,
+            android.R.color.holo_blue_bright,
+            android.R.color.holo_green_dark,
+            android.R.color.holo_orange_dark,
+            android.R.color.holo_blue_bright,
+            android.R.color.black,
+            };
+    final int [] names = new int[]{
+            R.id.view01,
+            R.id.view02,
+            R.id.view03,
+            R.id.view04,
+            R.id.view05,
+            R.id.view06};
+    TextView[] views = new TextView[names.length];
+    Handler handler = new Handler(){
+        @Override
+    public void handleMessage(Message msg){
+            //表明消息来自本程序所发
+            if (msg.what == 0x123){
+                for (int i = 0; i<names.length; i++){
+                    views[i].setBackgroundResource(colors[(i + currentColor) % names.length ]);
+                }
+                currentColor++;
+            }
+            super.handleMessage(msg);
+        }
+    };
+     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        setContentView(R.layout.frame_demo);
+         for(int i = 0; i < names.length; i ++){
+             views[i] = (TextView) findViewById(names[i]);
+         }
+         //定义一个线程周期性地改变currentColor变量值
+         new Timer().schedule(new TimerTask() {
+             @Override
+             public void run() {
+                 //发送一条空消息通知系统改变6个TextView组件的背景颜色
+                 handler.sendEmptyMessage(0x123);
+             }
+         },0,200);
     }
 }
